a983683460b5aec7705e4b17409630fa1fb58bc2
dfeiAlex <20221937@dfei.ie>
2023-01-31 14:02:37 +0000

    Initial commit

---
+++ appSnakeAndLadders/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("appSnakeAndLadders")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("DFEi")>
+<Assembly: AssemblyProduct("appSnakeAndLadders")>
+<Assembly: AssemblyCopyright("Copyright © DFEi 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e1131062-56e4-4af9-80ac-dd3edb7af0a7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
